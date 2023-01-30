76ed1c51757f1d2a1d3e7737ac3c4bca9a663bfa
Lucas García <lucas531@hotmail.com>
2023-01-30 18:53:40 -0300

    CV proyecto final

---
+++ Js/index.js
@@ -0,0 +1,5 @@
+
+document.getElementById("botonExperiencia").addEventListener("click", function () {
+    document.getElementById("experienciaLab").style.display = "block";
+    document.getElementById("botonExperiencia").style.backgroundColor = "blue";
+});
